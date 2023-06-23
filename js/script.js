d79bf3b9921afff56664aa82eb9a675bb1585946
FraFra2 <kekkino41@gmail.com>
2023-06-23 15:31:23 +0200

    ADD array img

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ let img = [
     '<img class="img-1" src="img/05.webp">',
 ];
 
+let selImg = pickRandomFromArray(img);
+
+for (let index = 0; index < img.length; index++) {
+    const element = img[index];
+    console.log(element);
+
+}
+
+function pickRandomFromArray(arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+}
