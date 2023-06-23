43f4ae57c8874e940a2f93dc205b60cae5385a9d
FraFra2 <kekkino41@gmail.com>
2023-06-23 15:27:17 +0200

    ADD Milestone 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let img = [
+    '<img class="img-1" src="img/01.webp">',
+    '<img class="img-1" src="img/02.webp">',
+    '<img class="img-1" src="img/03.webp">',
+    '<img class="img-1" src="img/04.webp">',
+    '<img class="img-1" src="img/05.webp">',
+];
+
